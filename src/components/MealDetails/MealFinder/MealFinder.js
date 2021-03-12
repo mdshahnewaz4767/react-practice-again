f1bfec999e4c7b6bebe280c620bdceb4cf96e3a0
shahnewaz601 <shahnewaz601@gmail.com>
2021-03-13 00:44:22 +0600

    Implement search functionality in a react application

--- src/components/MealDetails/MealFinder/MealFinder.js
+++ src/components/MealDetails/MealFinder/MealFinder.js
@@ -9,19 +9,24 @@ const MealFinder = () => {
         setSearch(event.target.value);
     }
     useEffect(() => [
-        fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata")
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
         .then(res => res.json())
         .then(data => {
           setMeals(data.meals)
           console.log(data.meals);
         })
-    ], [])
+    ], [search])
     return (
         <div>
             <h1>Meal Finder Delicious Food</h1>
             <input type="text" onChange={handleChanged} placeholder="search food"/>
             <p>Searching: {search}</p>
-            <p>Meal Found: {meals.length}</p>
+            <p>Meal Found: {meals?.length || 0}</p>
+            <ul>
+                {
+                    meals?.map(meal => <li key={meal.idMeal}>{meal.strMeal}</li>)
+                }
+            </ul>
         </div>
     );
 };
