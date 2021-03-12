17f893e64084cad61c317ba3d9139751e16870a7
shahnewaz601 <shahnewaz601@gmail.com>
2021-03-12 23:23:49 +0600

    use another api

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 function App() {
 
   const [users, setUsers] = useState([])
+  const [singleUser, setSingleUser] = useState({})
+  const [randomUser, setRandomUser] = useState({})
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -12,9 +14,29 @@ function App() {
       setUsers(data)
       // console.log(data);
     })
-  })
+
+    //Single users
+    fetch("https://jsonplaceholder.typicode.com/users/1")
+    .then(res => res.json())
+    .then(data => {
+      setSingleUser(data)
+      // console.log(data);
+    })
+
+    //Random users
+    fetch("https://randomuser.me/api/")
+    .then(res => res.json())
+    .then(data => {
+      setRandomUser(data.results[0])
+      console.log(data.results[0]);
+    })
+  }, [])
   return (
     <div className="App">
+      <h1>{singleUser.name}</h1>
+      <h1>Random Gender: {randomUser.gender}</h1>
+      <h1>Random Name: {randomUser.name && randomUser.name.first}</h1>
+      <h2>Random Name: {randomUser.name?.first}</h2>
       {
         users.map(user => 
           <li key={user.id}>{user.name}</li>)
