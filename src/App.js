924904d38dbe4efa575e8f122452c8584a02ba99
shahnewaz601 <shahnewaz601@gmail.com>
2021-03-12 22:28:05 +0600

    Load data using hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res => res.json())
+    .then(data => {
+      setUsers(data)
+      // console.log(data);
+    })
+  })
   return (
     <div className="App">
-     
+      {
+        users.map(user => 
+          <li key={user.id}>{user.name}</li>)
+      }
+    
     </div>
   );
 }
